5cdb54e36e50ae98d495c9f02314f2959bd133fb
Thomas Arnold <ybn4aq@virginia.edu>
2024-04-03 16:11:15 -0400

    platform logos

--- web/src/gameRank/GameGetter.php
+++ web/src/gameRank/GameGetter.php
@@ -66,14 +66,14 @@ class GameGetter {
                 $builder
                 ->where("id = $gameID")
                 ->fields("name, summary, screenshots.url, cover.url, platforms.*, involved_companies.*, genres.name,
-                involved_companies.company.name, involved_companies.company.logo.url, release_dates.human")
+                involved_companies.company.name, involved_companies.company.logo.url, release_dates.human, platforms.platform_logo.url")
                 ->build()
             );
         }
         catch (Exception $e) {
             echo $e->getMessage();
         }
-        var_dump($query);
+        // var_dump($query);
         // Simple ones
         $ret["name"] = $query[0]->name;
         $ret["summary"] = $query[0]->summary;
@@ -105,13 +105,15 @@ class GameGetter {
         // Getting platforms
         $platforms_query = $query[0]->platforms;
         $platforms = [];
+        $platform_logos = [];
         for ($i = 0; $i < count($platforms_query); $i++) {
             $platforms[] = $platforms_query[$i]->name;
+            $platform_logos[] = str_replace("t_thumb", "t_720p", $platforms_query[$i]->platform_logo->url);
         }
         $ret["platforms"] = $platforms;
+        $ret["platform_logos"] = $platform_logos;
         // Getting release date
         $ret["release_date"] = $query[0]->release_dates[0]->human;
-
         return $ret;
     }
 
