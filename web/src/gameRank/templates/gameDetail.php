b8d46fb1c4fba7e7514e4af9066e7034d8db5c11
Thomas Arnold <ybn4aq@virginia.edu>
2024-04-03 17:07:18 -0400

    basic CSS

--- web/src/gameRank/templates/gameDetail.php
+++ web/src/gameRank/templates/gameDetail.php
@@ -11,16 +11,135 @@
     <meta name="description" content="Game Rank">
     <meta name="keywords" content="video games, games">
     <title>Game Rank Detail</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f8f9fa;
+            color: #343a40;
+        }
+
+        .container {
+            padding-top: 20px;
+            padding-bottom: 20px;
+        }
+
+        .card {
+            margin-bottom: 20px;
+            width: 95%;
+            height: 100%;
+            padding: 5px;
+        }
+
+        .card-title {
+            margin-bottom: 10px;
+        }
+
+        .card-text {
+            margin-bottom: 20px;
+        }
+
+        .list-group-item {
+            border: none;
+            padding: 8px 0;
+        }
+
+        .screenshot-img {
+            max-width: 100%;
+            height: auto;
+            margin-bottom: 20px;
+        }
+
+        .company-logo {
+            max-width: 100px;
+            max-height: 50px;
+            margin-right: 10px;
+            vertical-align: middle;
+        }
+
+        .console-logo {
+            max-width: 100px;
+            max-height: 50px;
+        }
+
+        .game-img {
+            height: 25vh;
+            object-fit: fill;
+        }
+
+
+    </style>
+    <?php
+    $gameID = $_GET["id"];
+    $gameInfo = $this->gameGetter->getGameDetail($gameID);
+    $name = $gameInfo["name"];
+    $summary = $gameInfo["summary"];
+    $cover = $gameInfo["cover"];
+    $screenshots = $gameInfo["screenshots"];
+    $genres = $gameInfo["genres"];
+    $company = $gameInfo["company"];
+    $company_logo = $gameInfo["company_logo"];
+    $platforms = $gameInfo["platforms"];
+    $platform_logos = $gameInfo["platform_logos"];
+    $release_date = $gameInfo["release_date"];
+    ?>
 </head>
 <body>
-<?php
-include "navbar.php";
-$gameID = $_GET["id"];
-$gameInfo = $this->gameGetter->getGameDetail($gameID);
-var_dump($gameInfo);
+<?php include "navbar.php"; ?>
+<div class="container">
+    <div class="row">
+        <div class="col-md-4">
+            <div class="card h-100">
+                <img src="<?= $cover ?>" alt="<?= $name ?> cover" class="game-img card-img-top"/>
+                <div class="card-body">
+                    <h2 class="card-title"> <?= $name ?> </h2>
+                    <p class="card-text"> <?= $summary ?></p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <ul class="list-group">
+                <li class="list-group-item">
+                    Release Date: <?= $release_date ?>
+                </li>
+                <li class="list-group-item">
+                    Developer: <?= $company ?>
+                    <img src="<?= $company_logo ?>" alt="<?= $company ?> logo " class='company-logo'"/>
+                </li>
+                <li class="list-group-item">
+                    Genres:
+                    <?php
+                    for ($i = 0; $i < count($genres); $i++) {
+                        echo $genres[$i] . ", ";
+                    }
+                    ?>
+                </li>
+                <li class="list-group-item">
+                    Platforms:
+                    <?php
+                    for ($i = 0; $i < count($platforms); $i++) {
+                        echo "<li>" . $platforms[$i] . "<img src='$platform_logos[$i]' alt='$platforms[$i] logo' class='console-logo'/>" . "</li>";
+                    }
+                    ?>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <h3> Screenshots </h3>
+            <div class="row">
+                <?php
+                for ($i = 0; $i < count($screenshots); $i++) {
+                    echo "<div class='col-md-4'>";
+                    echo "<img src='$screenshots[$i]' alt='$name screenshot' class='screenshot-img'/>";
+                    echo "</div>";
+                }
+                ?>
+            </div>
+        </div>
+    </div>
+</div>
 
-
-?>
 
 <!-- Include Bootstrap JS -->
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
