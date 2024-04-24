90fe09c63e8dfa7cdec532e1c19fc11ece618def
Matthew Haid <wsd6vn@virginia.edu>
2024-04-24 10:25:37 -0400

    homePage has js

--- web/src/gameRank/templates/homePage.php
+++ web/src/gameRank/templates/homePage.php
@@ -20,62 +20,20 @@
 <body>
   <!-- Navbar -->
   <div class="bg-image">
-    <header class="p-3 mb-3 border-bottom navbar-dark bg-dark">
-      <?php if (isset($_SESSION['user'])): ?>
-      <div class="col-md-11 offset-md-1">
-        <?php else: ?>
-          <div class="col-md-10 offset-md-1">
-            <?php endif; ?>
+  <header class="p-3 mb-3 border-bottom navbar-dark bg-dark">
+    <div class="js-dynamic-col">
         <div class="d-flex flex-wrap align-items-center justify-content-between">
-          <a href="?command=showHomepage"
-            class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-            <img src="images/controllerLogo.png" alt="Logo" height="30" style="margin-right: 10px; margin-top: -10px;">
-            <span class="fs-3 logo">Game Rank</span>
-          </a>
-          <div class="d-flex flex-wrap align-items-center">
-            <?php
-            if (isset($_SESSION['user'])) {
-              echo '<ul class="nav col-12 col-lg-auto mb-2 justify-content-center mb-md-0 mr-1">
-            <li>
-                <a href="?command=showHomepage" class="nav-link px-2 link-body-emphasis">Home</a>
-            </li>
-            <li>
-                <a href="?command=showGroups" class="nav-link px-2 link-body-emphasis" id="fix-me">Your
-                    Groups</a>
-            </li>
-            <li>
-                <a href="?command=showJoinGroup" class="nav-link px-2 link-body-emphasis">Join Group</a>
-            </li>
-            <li>
-                <a href="?command=showCreateGroup" class="nav-link px-2 link-body-emphasis">Create Group</a>
-            </li>
-        </ul>
-
-        <div class="dropdown text-end">
-            <a href="#" class="d-block link-body-emphasis text-decoration-none dropdown-toggle"
-                data-bs-toggle="dropdown" aria-expanded="false">
-                <img src="images/mario.png" alt="mdo" width="32" height="32" class="rounded-circle">
+            <a href="?command=showHomepage"
+               class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+                <img src="images/controllerLogo.png" alt="Logo" height="30" style="margin-right: 10px; margin-top: -10px;">
+                <span class="fs-3 logo">Game Rank</span>
             </a>
-            <ul class="dropdown-menu text-small">
-                <li><a class="dropdown-item" href="?command=showCreateGroup">Create Group</a></li>
-                <li>
-                    <a class="dropdown-item" href="?command=showGroups">Your Groups</a>
-                </li>
-                <li><a class="dropdown-item" href="#">Profile</a></li>
-                <li>
-                    <hr class="dropdown-divider">
-                </li>
-                <li><a class="dropdown-item" href="?command=logout">Sign out</a></li>
-            </ul>';
-            } else {
-              echo '<a class="btn btn-outline-primary me-2" href="?command=showLogin">Log In</a>';
-              echo '<a class="btn btn-primary" href="?command=showSignup">Register</a>';
-            }
-            ?>
-          </div>
+            <div class="d-flex flex-wrap align-items-center js-nav">
+            </div>
         </div>
-      </div>
-    </header>
+    </div>
+</header>
+
 
 
 
@@ -180,6 +138,44 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
     integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy"
     crossorigin="anonymous"></script>
+    <script>
+      document.addEventListener('DOMContentLoaded', function() {
+    const userLoggedIn = <?php echo isset($_SESSION['user']) ? 'true' : 'false'; ?>;
+    const dynamicCol = document.querySelector('.js-dynamic-col');
+    const navPlaceholder = document.querySelector('.js-nav');
+
+    if (userLoggedIn) {
+        dynamicCol.className = 'col-md-11 offset-md-1';
+
+        navPlaceholder.innerHTML = `
+            <ul class="nav col-12 col-lg-auto mb-2 justify-content-center mb-md-0 mr-1">
+                <li><a href="?command=showHomepage" class="nav-link px-2 link-body-emphasis">Home</a></li>
+                <li><a href="?command=showGroups" class="nav-link px-2 link-body-emphasis" id="fix-me">Your Groups</a></li>
+                <li><a href="?command=showJoinGroup" class="nav-link px-2 link-body-emphasis">Join Group</a></li>
+                <li><a href="?command=showCreateGroup" class="nav-link px-2 link-body-emphasis">Create Group</a></li>
+            </ul>
+            <div class="dropdown text-end">
+                <a href="#" class="d-block link-body-emphasis text-decoration-none dropdown-toggle"
+                   data-bs-toggle="dropdown" aria-expanded="false">
+                    <img src="images/mario.png" alt="mdo" width="32" height="32" class="rounded-circle">
+                </a>
+                <ul class="dropdown-menu text-small">
+                    <li><a class="dropdown-item" href="?command=showCreateGroup">Create Group</a></li>
+                    <li><a class="dropdown-item" href="?command=showGroups">Your Groups</a></li>
+                    <li><a class="dropdown-item" href="#">Profile</a></li>
+                    <hr class="dropdown-divider">
+                    <li><a class="dropdown-item" href="?command=logout">Sign out</a></li>
+                </ul>`;
+
+    } else {
+        dynamicCol.className = 'col-md-10 offset-md-1';
+
+        navPlaceholder.innerHTML = `
+            <a class="btn btn-outline-primary me-2" href="?command=showLogin">Log In</a>
+            <a class="btn btn-primary" href="?command=showSignup">Register</a>`;
+    }
+});
+    </script>
 </body>
 
 </html>
